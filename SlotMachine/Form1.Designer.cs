ae4ec035581d347e0b39c6f1d50d6d1fd512658d
Mar7inD <Dotev32@gmail.com>
2022-07-21 17:56:19 +0200

    First commit

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SlotC = new System.Windows.Forms.PictureBox();
+            this.SlotA = new System.Windows.Forms.PictureBox();
+            this.SlotB = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMoney = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotB)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SlotC
+            // 
+            this.SlotC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SlotC.Image = global::SlotMachine.Properties.Resources.apple;
+            this.SlotC.Location = new System.Drawing.Point(280, 19);
+            this.SlotC.Name = "SlotC";
+            this.SlotC.Size = new System.Drawing.Size(102, 116);
+            this.SlotC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SlotC.TabIndex = 0;
+            this.SlotC.TabStop = false;
+            // 
+            // SlotA
+            // 
+            this.SlotA.BackColor = System.Drawing.SystemColors.Control;
+            this.SlotA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SlotA.Image = global::SlotMachine.Properties.Resources.apple;
+            this.SlotA.Location = new System.Drawing.Point(48, 19);
+            this.SlotA.Name = "SlotA";
+            this.SlotA.Size = new System.Drawing.Size(102, 116);
+            this.SlotA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SlotA.TabIndex = 1;
+            this.SlotA.TabStop = false;
+            // 
+            // SlotB
+            // 
+            this.SlotB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SlotB.Image = global::SlotMachine.Properties.Resources.apple;
+            this.SlotB.Location = new System.Drawing.Point(164, 19);
+            this.SlotB.Name = "SlotB";
+            this.SlotB.Size = new System.Drawing.Size(102, 116);
+            this.SlotB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SlotB.TabIndex = 2;
+            this.SlotB.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount to play :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Money :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2 Match =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "3 Match =";
+            // 
+            // lblMoney
+            // 
+            this.lblMoney.AutoSize = true;
+            this.lblMoney.Location = new System.Drawing.Point(298, 161);
+            this.lblMoney.Name = "lblMoney";
+            this.lblMoney.Size = new System.Drawing.Size(13, 13);
+            this.lblMoney.TabIndex = 8;
+            this.lblMoney.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 175);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5$";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(61, 175);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "10$";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(110, 175);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(43, 17);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "15$";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Location = new System.Drawing.Point(280, 138);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(100, 20);
+            this.txtMoney.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(247, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Add :";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(385, 136);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(41, 23);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "$";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(90, 275);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 18;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(370, 275);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 13);
+            this.lbl3.TabIndex = 19;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SlotA);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lbl2);
+            this.groupBox1.Controls.Add(this.lblMoney);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.SlotB);
+            this.groupBox1.Controls.Add(this.SlotC);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtMoney);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(23, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 313);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Slot Machine";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 485);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SlotC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotB)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SlotC;
+        private System.Windows.Forms.PictureBox SlotA;
+        private System.Windows.Forms.PictureBox SlotB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblMoney;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(0, 7);
+        }
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int a = 5, g = 10, h = 15;
+            int l = 0;
+            int k = Int32.Parse(lblMoney.Text);
+          
+            if (k <= 4) 
+            { MessageBox.Show("Not enough money"); }
+            
+            else
+            {
+                SlotA.Image = SlotMachine.Properties.Resources.animation;
+                SlotB.Image = SlotMachine.Properties.Resources.animation;
+                SlotC.Image = SlotMachine.Properties.Resources.animation;
+                if (radioButton1.Checked) 
+                { l = k - a; lbl2.Text = "10"; lbl3.Text = "15"; }
+                
+                else if (radioButton2.Checked) 
+                { l = k - g; lbl2.Text = "20"; lbl3.Text = "30"; }
+                
+                else if (radioButton3.Checked) 
+                { l = k - h; lbl2.Text = "30"; lbl3.Text = "45"; }
+                lblMoney.Text = l.ToString();
+            }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                int l = 0;
+                int k = Int32.Parse(lblMoney.Text);
+                
+                Random random = new Random();
+                int m = random.Next(0, 7);
+                int n = random.Next(0, 7);
+                int b = random.Next(0, 7);
+
+                switch (m)
+                {
+                    
+                case 0:
+                        SlotA.Image = SlotMachine.Properties.Resources.apple;
+                    SlotA.Tag = "8";
+                    break;
+                   
+                case 1:
+                        SlotA.Image = SlotMachine.Properties.Resources.banana;
+                    SlotA.Tag = "1";
+                    break;
+                   
+                case 2:
+                        SlotA.Image = SlotMachine.Properties.Resources.bar;
+                    SlotA.Tag = "2";
+                    break;
+                   
+                case 3:
+                        SlotA.Image = SlotMachine.Properties.Resources.cherry;
+                    SlotA.Tag = "3";
+                    break;
+                   
+                case 4:
+                        SlotA.Image = SlotMachine.Properties.Resources.grape;
+                    SlotA.Tag = "4";
+                    break;
+
+                case 5:
+                        SlotA.Image = SlotMachine.Properties.Resources.lemon;
+                    SlotA.Tag = "5";
+                    break;
+                   
+                case 6:
+                        SlotA.Image = SlotMachine.Properties.Resources.melon;
+                    SlotA.Tag = "6";
+                    break;
+                   
+                case 7:
+                        SlotA.Image = SlotMachine.Properties.Resources.pear;
+                    SlotA.Tag = "7";
+                    break;
+                   
+                default:
+                        SlotA.Image = SlotMachine.Properties.Resources.animation;
+                    SlotA.Tag = "0";
+                    break;
+                }
+
+
+                switch (n)
+                {
+                   
+                case 0:
+                        SlotB.Image = SlotMachine.Properties.Resources.apple;
+                    SlotB.Tag = "8";
+                    break;
+                  
+                case 1:
+                        SlotB.Image = SlotMachine.Properties.Resources.banana;
+                    SlotB.Tag = "1";
+                    break;
+                   
+                case 2:
+                        SlotB.Image = SlotMachine.Properties.Resources.bar;
+                    SlotB.Tag = "2";
+                    break;
+                    
+                case 3:
+                        SlotB.Image = SlotMachine.Properties.Resources.cherry;
+                    SlotB.Tag = "3";
+                    break;
+                  
+                case 4:
+                        SlotB.Image = SlotMachine.Properties.Resources.grape;
+                    SlotB.Tag = "4";
+                    break;
+                   
+                case 5:
+                        SlotB.Image = SlotMachine.Properties.Resources.lemon;
+                    SlotB.Tag = "5";
+                    break;
+                    
+                case 6:
+                        SlotB.Image = SlotMachine.Properties.Resources.melon;
+                    SlotB.Tag = "6";
+                    break;
+                    
+                case 7:
+                        SlotB.Image = SlotMachine.Properties.Resources.pear;
+                    SlotB.Tag = "7";
+                    break;
+                    
+                default:
+                        SlotB.Image = SlotMachine.Properties.Resources.animation;
+                    SlotB.Tag = "0";
+                    break;
+                }
+                switch (b)
+                {
+                
+                case 0:
+                        SlotC.Image = SlotMachine.Properties.Resources.apple;
+                    SlotC.Tag = "8";
+                        break;
+                
+                case 1:
+                        SlotC.Image = SlotMachine.Properties.Resources.banana;
+                    SlotC.Tag = "1";
+                    break;
+                    
+                case 2:
+                        SlotC.Image = SlotMachine.Properties.Resources.bar;
+                    SlotC.Tag = "2";
+                    break;
+                    
+                case 3:
+                        SlotC.Image = SlotMachine.Properties.Resources.cherry;
+                    SlotC.Tag = "3";
+                    break;
+                
+                case 4:
+                        SlotC.Image = SlotMachine.Properties.Resources.grape;
+                    SlotC.Tag = "4";
+                    break;
+                    
+                case 5:
+                        SlotC.Image = SlotMachine.Properties.Resources.lemon;
+                    SlotC.Tag = "5";
+                    break;
+                    
+                case 6:
+                        SlotC.Image = SlotMachine.Properties.Resources.melon;
+                    SlotC.Tag = "6";
+                    break;
+                
+                case 7:
+                        SlotC.Image = SlotMachine.Properties.Resources.pear;
+                    SlotC.Tag = "7";
+                    break;
+                    
+                default:
+                        SlotB.Image = SlotMachine.Properties.Resources.animation;
+                    SlotC.Tag = "0";
+                    break;
+            }
+            if (radioButton1.Checked)
+            {
+                if (SlotA.Tag == SlotB.Tag || SlotA.Tag == SlotC.Tag || SlotC.Tag == SlotB.Tag)
+                {
+                    if (SlotA.Tag == SlotB.Tag && SlotA.Tag == SlotC.Tag && SlotC.Tag == SlotB.Tag) { l = l + 15; }
+                    else { l = l + 10; }
+                }
+
+            }
+
+            else if (radioButton2.Checked)
+            {
+                if (SlotA.Tag == SlotB.Tag || SlotA.Tag == SlotC.Tag || SlotC.Tag == SlotB.Tag) 
+
+                {
+                    if (SlotA.Tag == SlotB.Tag && SlotA.Tag == SlotC.Tag && SlotC.Tag == SlotB.Tag) { l = l + 30; }
+                    else { l = l + 20; }
+                }
+            }
+
+            else if (radioButton3.Checked)
+            {
+                if (SlotA.Tag == SlotB.Tag || SlotA.Tag == SlotC.Tag || SlotC.Tag == SlotB.Tag) 
+
+                {
+                    if (SlotA.Tag == SlotB.Tag && SlotA.Tag == SlotC.Tag && SlotC.Tag == SlotB.Tag) { l = l + 45; }
+                    else { l = l + 30; }
+                }
+            }
+            l = k + l;
+            lblMoney.Text = l.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int k = Int32.Parse(lblMoney.Text);
+            int n = Int32.Parse(txtMoney.Text);
+            int p = 0;
+            if (n <= 0 || n > 1000 || n % 5 != 0) 
+            { MessageBox.Show("The value you want to add is lower or equal to 0 or is higher than 1000 or it can't be divided by 5"); }
+            
+            else { p = k + n; lblMoney.Text = p.ToString(); int u = Int32.Parse(lblMoney.Text);
+                if (p > 1000) { MessageBox.Show("The value of your money is higher than 1000 and we exported it to your account."+"We transfered "+ p +" $ to your bank account");lblMoney.Text = "0"; }
+                    
+                        }
+            
+            txtMoney.Text = "";
+        }
+    }
+}
+
